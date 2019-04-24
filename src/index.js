909c7c47742d7bc5790ce00ceec6e07e03fb1e0e
L <manguluka@gmx.com>
2019-04-24 13:10:56 -0700

    refactor SplitterLayout style

--- src/index.js
+++ src/index.js
@@ -1,10 +1,14 @@
 import React from "react";
 
 import SplitterLayout from "react-splitter-layout";
-import "react-splitter-layout/lib/index.css"
-import "./styles.css"
 import CodeEditor from "@lilonga/code-editor";
 
+class ResizeAceHack {
+  constructor(editor){
+    setTimeout(_=>window.dispatchEvent(new Event('resize')),500)
+    editor.resize()
+  }
+}
 export default class LiveEditor extends React.Component {
   constructor(props) {
     super(props);
@@ -17,20 +21,84 @@ export default class LiveEditor extends React.Component {
     return (
       <div
         style={{
-          width: this.props.width ,
-          height: this.props.height ,
+          width: this.props.width,
+          height: this.props.height,
           overflow: "scroll"
         }}
       >
+        <style
+          dangerouslySetInnerHTML={{
+            __html: `
+
+.splitter-layout {
+  position: absolute;
+  display: flex;
+  flex-direction: row;
+  width: 100%;
+  height: 100%;
+  overflow: hidden;
+}
+
+.splitter-layout .layout-pane {
+  position: relative;
+  flex: 0 0 auto;
+  overflow: auto;
+}
+
+.splitter-layout .layout-pane.layout-pane-primary {
+  flex: 1 1 auto;
+}
+
+.splitter-layout > .layout-splitter {
+  flex: 0 0 auto;
+  width: 4px;
+  height: 100%;
+  cursor: col-resize;
+  background-color: #ccc;
+}
+
+.splitter-layout .layout-splitter:hover {
+  background-color: #bbb;
+}
+
+.splitter-layout.layout-changing {
+  cursor: col-resize;
+}
+
+.splitter-layout.layout-changing > .layout-splitter {
+  background-color: #aaa;
+}
+
+.splitter-layout.splitter-layout-vertical {
+  flex-direction: column;
+}
+
+.splitter-layout.splitter-layout-vertical.layout-changing {
+  cursor: row-resize;
+}
+
+.splitter-layout.splitter-layout-vertical > .layout-splitter {
+  width: 100%;
+  height: 4px;
+  cursor: row-resize;
+}
+
+.splitter-layout{
+  position: relative !important;
+}
+`
+          }}
+        />
         <SplitterLayout vertical={this.props.vertical || false}>
           <CodeEditor
             {...this.props.editorProps}
+            plugins={this.props.editorProps.plugins ? [ResizeAceHack,...this.props.editorProps.plugins] : [ResizeAceHack] }
             height={this.props.editorProps.height || "400px"}
-            width={this.props.editorProps.width || "400px"}
-            mode = {this.props.editorProps.mode}
+            width={this.props.editorProps.width || "100%"}
+            mode={this.props.editorProps.mode}
             onChange={input => {
               this.setState({ input });
-              this.props.onInputChange(input)
+              this.props.onInputChange(input);
             }}
             code={this.state.input}
           />
